fe67e34de066a43951e06bc98f7ecdf71ae6d5da
revenuxe <revenuxe@gmail.com>
2026-05-14 14:36:55 +0530

    Fix Next metadata sitemap route

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js", "tsx", "ts", "jsx", "js"],
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
